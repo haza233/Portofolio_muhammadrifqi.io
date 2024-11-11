d22ad1094a3dc9e0e8f579c0847ab3506effcbe9
haza305 <rifqiajja55@gmail.com>
2024-11-11 16:16:37 +0700

    wow

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,6 +1,8 @@
 import eight from "../assets/svg/projects/eight.svg";
 import seven from "../assets/svg/projects/seven.svg";
 import six from "../assets/svg/projects/six.svg";
+import nine from "../assets/svg/projects/nine.svg";
+import ten from "../assets/svg/projects/ten.svg";
 
 export const projectsData = [
   {
@@ -8,8 +10,8 @@ export const projectsData = [
     projectName: "Dasboard Test Drive Telkomsel",
     projectDesc: "This project is about Telkomsel dashboard test drive.",
     tags: ["React", "React Bootstrap"],
-    code: "",
-    demo: "https://dashboarddttsel.com/",
+    code: "https://drive.google.com/file/d/1xNCZqXRrxyYrM4CEpiEpnpmh_AN669p0/view?usp=sharing",
+    demo: "https://drive.google.com/file/d/1xNCZqXRrxyYrM4CEpiEpnpmh_AN669p0/view?usp=sharing",
     image: six,
   },
   {
@@ -18,18 +20,36 @@ export const projectsData = [
     projectDesc:
       "Where I provide support for absenteeism repairs and adding requests from user's using Laravel",
     tags: ["Laravel Lumen", "Material Ui"],
-    code: "",
-    demo: "https://picosd.com/",
+    code: "https://drive.google.com/file/d/1kf1We0CCURX0ZkC-u-NDfjYMXM5_u-vO/view?usp=sharing",
+    demo: "https://drive.google.com/file/d/1kf1We0CCURX0ZkC-u-NDfjYMXM5_u-vO/view?usp=sharing",
     image: seven,
   },
   {
     id: 3,
+    projectName: "Dasboard Robin Telkomsel",
+    projectDesc: "This project about travel agency management.",
+    tags: ["Ci3", "CSS", "bostrap4"],
+    code: "https://drive.google.com/file/d/1v9dbbFbajybERIwNDBxGpHuu1g6_Qxz6/view?usp=sharing",
+    demo: "https://drive.google.com/file/d/1v9dbbFbajybERIwNDBxGpHuu1g6_Qxz6/view?usp=sharing",
+    image: eight,
+  },
+  {
+    id: 4,
     projectName: "Website Biro Jasa Global Jaya sejahtera",
     projectDesc: "This project about travel agency management.",
     tags: ["React", "CSS", "Material Ui"],
     code: "",
     demo: "https://globaljayasejahtera.com/",
-    image: eight,
+    image: nine,
+  },
+  {
+    id: 5,
+    projectName: "Website E-office PT Permodalan Nasional Madan (PNM)",
+    projectDesc: "This project about travel agency management.",
+    tags: ["CI3", "CSS", "Boostrap3"],
+    code: "",
+    demo: "",
+    image: ten,
   },
 ];
 
